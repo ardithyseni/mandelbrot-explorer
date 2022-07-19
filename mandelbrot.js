0e8b308a15f4358c365ebae7a72d1f3e375b8d63
ardithyseni <ah49393@ubt-uni.net>
2022-07-19 15:22:13 +0200

    added the function to draw the mandelbrot

--- mandelbrot.js
+++ mandelbrot.js
@@ -174,3 +174,262 @@ function readHashTag()
 
     return redraw;
 }
+
+/*
+ * Return number with metric units
+ */
+function metric_units(number) 
+{
+    var unit = ["", "k", "M", "G", "T", "P", "E"];
+    var mag = Math.ceil((1 + Math.log(number) / Math.log(10)) / 3);
+    return "" + (number / Math.pow(10, 3 * (mag - 1))).toFixed(2) + unit[mag];
+}
+
+/*
+ * Convert hue-saturation-value/luminosity to RGB.
+ *
+ * Input ranges:
+ *   H =   [0, 360] (integer degrees)
+ *   S = [0.0, 1.0] (float)
+ *   V = [0.0, 1.0] (float)
+ */
+function hsv_to_rgb(h, s, v) 
+{
+    if (v > 1.0) v = 1.0;
+    var hp = h / 60.0;
+    var c = v * s;
+    var x = c * (1 - Math.abs((hp % 2) - 1));
+    var rgb = [0, 0, 0];
+
+    if (0 <= hp && hp < 1) rgb = [c, x, 0];
+    if (1 <= hp && hp < 2) rgb = [x, c, 0];
+    if (2 <= hp && hp < 3) rgb = [0, c, x];
+    if (3 <= hp && hp < 4) rgb = [0, x, c];
+    if (4 <= hp && hp < 5) rgb = [x, 0, c];
+    if (5 <= hp && hp < 6) rgb = [c, 0, x];
+
+    var m = v - c;
+    rgb[0] += m;
+    rgb[1] += m;
+    rgb[2] += m;
+
+    rgb[0] *= 255;
+    rgb[1] *= 255;
+    rgb[2] *= 255;
+    return rgb;
+}
+
+
+/*
+ * Adjust aspect ratio based on plot ranges and canvas dimensions.
+ */
+function adjustAspectRatio(xRange, yRange, canvas)
+{
+  var ratio = Math.abs(xRange[1]-xRange[0]) / Math.abs(yRange[1]-yRange[0]);
+  var sratio = canvas.width/canvas.height;
+  if ( sratio>ratio ) {
+    var xf = sratio/ratio;
+    xRange[0] *= xf;
+    xRange[1] *= xf;
+      zoom[0] *= xf;
+  } else {
+    var yf = ratio/sratio;
+    yRange[0] *= yf;
+    yRange[1] *= yf;
+      zoom[1] *= yf;
+  }
+}
+
+function addRGB(v, w)
+{
+  v[0] += w[0];
+  v[1] += w[1];
+  v[2] += w[2];
+  v[3] += w[3];
+  return v;
+}
+
+function divRGB(v, div)
+{
+  v[0] /= div;
+  v[1] /= div;
+  v[2] /= div;
+  v[3] /= div;
+  return v;
+}
+
+/*
+ * Render the Mandelbrot set
+ */
+function draw(pickColor, superSamples)
+{
+  if ( lookAt === null ) lookAt = [-0.6, 0];
+  if ( zoom === null ) zoom = [zoomStart, zoomStart];
+
+  xRange = [lookAt[0]-zoom[0]/2, lookAt[0]+zoom[0]/2];
+  yRange = [lookAt[1]-zoom[1]/2, lookAt[1]+zoom[1]/2];
+
+  if ( reInitCanvas ) {
+    reInitCanvas = false;
+
+    canvas = $('canvasMandelbrot');
+    canvas.width  = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    ccanvas = $('canvasControls');
+    ccanvas.width  = window.innerWidth;
+    ccanvas.height = window.innerHeight;
+
+    ctx = canvas.getContext('2d');
+    img = ctx.createImageData(canvas.width, 1);
+
+    adjustAspectRatio(xRange, yRange, canvas);
+  }
+
+  var steps = parseInt($('steps').value, 10);
+
+  if ( $('autoIterations').checked ) {
+    var f = Math.sqrt(
+            0.001+2.0 * Math.min(
+              Math.abs(xRange[0]-xRange[1]),
+              Math.abs(yRange[0]-yRange[1])));
+
+    steps = Math.floor(223.0/f);
+    $('steps').value = String(steps);
+  }
+
+  var escapeRadius = Math.pow(parseFloat($('escapeRadius').value), 2.0);
+  var dx = (xRange[1] - xRange[0]) / (0.5 + (canvas.width-1));
+  var dy = (yRange[1] - yRange[0]) / (0.5 + (canvas.height-1));
+  var Ci_step = (yRange[1] - yRange[0]) / (0.5 + (canvas.height-1));
+
+  updateHashTag(superSamples, steps);
+  updateInfoBox();
+
+  // Only enable one render at a time
+  renderId += 1;
+
+  function drawLineSuperSampled(Ci, off, Cr_init, Cr_step)
+  {
+    var Cr = Cr_init;
+
+    for ( var x=0; x<canvas.width; ++x, Cr += Cr_step ) {
+      var color = [0, 0, 0, 255];
+
+      for ( var s=0; s<superSamples; ++s ) {
+        var rx = Math.random()*Cr_step;
+        var ry = Math.random()*Ci_step;
+        var p = iterateEquation(Cr - rx/2, Ci - ry/2, escapeRadius, steps);
+        color = addRGB(color, pickColor(steps, p[0], p[1], p[2]));
+      }
+
+      color = divRGB(color, superSamples);
+
+      img.data[off++] = color[0];
+      img.data[off++] = color[1];
+      img.data[off++] = color[2];
+      img.data[off++] = 255;
+    }
+  }
+
+  function drawLine(Ci, off, Cr_init, Cr_step)
+  {
+    var Cr = Cr_init;
+
+    for ( var x=0; x<canvas.width; ++x, Cr += Cr_step ) {
+      var p = iterateEquation(Cr, Ci, escapeRadius, steps);
+      var color = pickColor(steps, p[0], p[1], p[2]);
+      img.data[off++] = color[0];
+      img.data[off++] = color[1];
+      img.data[off++] = color[2];
+      img.data[off++] = 255;
+    }
+  }
+
+  function drawSolidLine(y, color)
+  {
+    var off = y*canvas.width;
+
+    for ( var x=0; x<canvas.width; ++x ) {
+      img.data[off++] = color[0];
+      img.data[off++] = color[1];
+      img.data[off++] = color[2];
+      img.data[off++] = color[3];
+    }
+  }
+
+  function render()
+  {
+    var start  = (new Date).getTime();
+    var startHeight = canvas.height;
+    var startWidth = canvas.width;
+    var lastUpdate = start;
+    var updateTimeout = parseFloat($('updateTimeout').value);
+    var pixels = 0;
+    var Ci = yRange[0];
+    var sy = 0;
+    var drawLineFunc = superSamples>1? drawLineSuperSampled : drawLine;
+    var ourRenderId = renderId;
+
+    var scanline = function()
+    {
+      if (    renderId != ourRenderId ||
+           startHeight != canvas.height ||
+            startWidth != canvas.width )
+      {
+        // Stop drawing
+        return;
+      }
+
+      drawLineFunc(Ci, 0, xRange[0], dx);
+      Ci += Ci_step;
+      pixels += canvas.width;
+      ctx.putImageData(img, 0, sy);
+
+      var now = (new Date).getTime();
+
+      /*
+       * Javascript is inherently single-threaded, and the way
+       * you yield thread control back to the browser is MYSTERIOUS.
+       *
+       * People seem to use setTimeout() to yield, which lets us
+       * make sure the canvas is updated, so that we can do animations.
+       *
+       * But if we do that for every scanline, it will take 100x longer
+       * to render everything, because of overhead.  So therefore, we'll
+       * do something in between.
+       */
+      if ( sy++ < canvas.height ) {
+        if ( (now - lastUpdate) >= updateTimeout ) {
+          // show the user where we're rendering
+          drawSolidLine(0, [255,59,3,255]);
+          ctx.putImageData(img, 0, sy);
+
+          // Update speed and time taken
+          var elapsedMS = now - start;
+          $('renderTime').innerHTML = (elapsedMS/1000.0).toFixed(1); // 1 comma
+
+          var speed = Math.floor(pixels / elapsedMS);
+
+          if ( metric_units(speed).substr(0,3)=="NaN" ) {
+            speed = Math.floor(60.0*pixels / elapsedMS);
+            $('renderSpeedUnit').innerHTML = 'minute';
+          } else
+            $('renderSpeedUnit').innerHTML = 'second';
+
+          $('renderSpeed').innerHTML = metric_units(speed);
+
+          // yield control back to browser, so that canvas is updated
+          lastUpdate = now;
+          setTimeout(scanline, 0);
+        } else
+          scanline();
+      }
+    };
+
+    // Disallow redrawing while rendering
+    scanline();
+  }
+
+  render();
+}
